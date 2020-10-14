98c75ec9117d3362acaa38ec4b13262b695e170c
your name here <johnobrien8642@gmail.com>
2020-10-14 14:37:16 -0400

    models ::AnswerChoice, ::Poll, ::Question, ::Response, ::User models created with associations

---
+++ app/models/response.rb
@@ -0,0 +1,13 @@
+
+
+class Response < ApplicationRecord
+    belongs_to :respondent,
+      class_name: :User,
+      foreign_key: :respondent_id,
+      primary_key: :id
+
+    belongs_to :answer_choice,
+      class_name: :AnswerChoice,
+      foreign_key: :answer_choice_id,
+      primary_key: :id
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+    User.destroy_all
+    Poll.destroy_all
+    Question.destroy_all
+    Response.destroy_all
+    AnswerChoice.destroy_all
+
+    u1 = User.create!(username: "BigBill")
+    u2 = User.create!(username: "Lefty")
+    u3 = User.create!(username: "Avalanche")
+
+    poll1 = Poll.create!(user_id: u1.id, title: "Candy Mechanics")
+
+    q1 = Question.create!(poll_id: poll1.id, body: 'How many licks to the center of a tootsie pop?')
+        ac1 = AnswerChoice.create!(question_id: q1.id, body: '1 licks')
+        ac2 = AnswerChoice.create!(question_id: q1.id, body: '2 licks')
+        ac3 = AnswerChoice.create!(question_id: q1.id, body: '3 licks')
+
+    q2 = Question.create!(poll_id: poll1.id, body: 'Which candy bar is the best?')
+        ac4 = AnswerChoice.create!(question_id: q2.id, body: 'Hershey''s Chocolate Bar')
+        ac5 = AnswerChoice.create!(question_id: q2.id, body: 'Reese''s Fast Break')
+        ac6 = AnswerChoice.create!(question_id: q2.id, body: 'Snickers')
+    
+    resp1 = Response.create!(respondent_id: u2.id, answer_choice_id: ac1.id)
+    resp2 = Response.create!(respondent_id: u3.id, answer_choice_id: ac3.id)
+    resp3 = Response.create!(respondent_id: u1.id, answer_choice_id: ac3.id)
+    resp4 = Response.create!(respondent_id: u1.id, answer_choice_id: ac5.id)
+end
